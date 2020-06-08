ca02b8a6aab7d4a883e55b927b2f6319770a7d37
Marcin <marcin.komorski@gmail.com>
2020-06-08 21:54:00 +0200

    +

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('http').createServer(handler)
 const fs = require('fs')
 const io = require('socket.io').listen(app)
 
-app.listen(8081);
+app.listen(8088);
 
 function handler(req, res) {
 	fs.readFile('index.html', function(err, data) {
@@ -12,8 +12,22 @@ function handler(req, res) {
 }
 
 io.sockets.on('connection', function(socket) {
-	socket.on('data', function(data) {
-		console.log("data!!")
-		socket.broadcast.emit('data', data);
+	socket.on('flights', function(data) {
+		console.log("data!!")		
 	});
+
+	var count = 0;
+	var coords = [51.505, -0.09];
+
+	setInterval(() => {
+		count++;
+		var value = count / 10000;
+		socket.broadcast.emit('flights', [
+			{
+				id: "fede123",
+				coords: [coords[0] + value, coords[1] + value]
+			}
+		]);	
+	}, 200)
+	
 });
