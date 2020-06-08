f5c9ad866a09e748fd7c22dd9e22b30a7bd81240
Marcin <marcin.komorski@gmail.com>
2020-06-08 21:55:12 +0200

    add

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ function handler(req, res) {
 	});
 }
 
-io.sockets.on('connection', function(socket) {
-	socket.on('flights', function(data) {
-		console.log("data!!")		
-	});
+// io.sockets.on('connection', function(socket) {
+// 	socket.on('flights', function(data) {
+// 		console.log("data!!")		
+// 	});
 
-	var count = 0;
-	var coords = [51.505, -0.09];
+// 	var count = 0;
+// 	var coords = [51.505, -0.09];
 
-	setInterval(() => {
-		count++;
-		var value = count / 10000;
-		socket.broadcast.emit('flights', [
-			{
-				id: "fede123",
-				coords: [coords[0] + value, coords[1] + value]
-			}
-		]);	
-	}, 200)
+// 	setInterval(() => {
+// 		count++;
+// 		var value = count / 10000;
+// 		socket.broadcast.emit('flights', [
+// 			{
+// 				id: "fede123",
+// 				coords: [coords[0] + value, coords[1] + value]
+// 			}
+// 		]);	
+// 	}, 200)
 	
-});
+// });
