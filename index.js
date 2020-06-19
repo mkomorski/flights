7f8e116f2ff60371e3a13c2144cec7c6fd3b6e05
Marcin <marcin.komorski@gmail.com>
2020-06-19 21:15:08 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const app = require('http').createServer(handler)
 const fs = require('fs')
 const io = require('socket.io').listen(app)
-
+const fetch = require('node-fetch');
+ 
 app.listen(8088);
 
 function handler(req, res) {
@@ -20,18 +21,36 @@ io.sockets.on('connection', function(socket) {
 		socket.broadcast.emit('flights', data);		
 	});
 
-	// var count = 0;
-	// var coords = [51.505, -0.09];
+	var count = 0;
+	var coords = [51.505, -0.09];
 
-	// setInterval(() => {
-	// 	count++;
-	// 	var value = count / 10000;
-	// 	socket.broadcast.emit('flights', [
-	// 		{
-	// 			id: "fede123",
-	// 			coords: [coords[0] + value, coords[1] + value]
+	// setInterval(async () => {
+		
+	// 	const response = await fetch('https://api.flightstats.com/flex/flightstatus/rest/v2/json/flightsNear/51.505/-0.09/200?appId=a3e62800&appKey=dacf07f407cbfcb481746b24d0b95705&maxFlights=120')
+	// 	const json = await response.json()
+		
+
+	// 	const output = json.flightPositions.map(p => {
+	// 		const pos = p.positions.pop()
+	// 		return {
+	// 			flightId: p.callsign,
+	// 			coords: [pos.lat, pos.lon]
 	// 		}
-	// 	]);	
-	// }, 200)
+			
+	// 	})
+
+	// 	console.log(output)
+	// 	socket.broadcast.emit('flights', output)
+
+
+		// count++;
+		// var value = count / 10000;
+		// socket.broadcast.emit('flights', [
+		// 	{
+		// 		id: "fede123",
+		// 		coords: [coords[0] + value, coords[1] + value]
+		// 	}
+		// ]);	
+	// }, 60000)
 	
 });
